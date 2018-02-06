bbaf6445a9a02439fab8f923208a66e3675f4f12
zhiyaohaha <bo5080110@163.com>
2018-02-06 18:02:05 +0800

    办卡进度完成

---
+++ src/components/scheduleList/scheduleList.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <ul class="scheduleList">
+      <li v-for="(scheduleListData, index) in scheduleListDatas">
+        <span class="bankCard">{{scheduleListData.bankCard}}</span>
+        <span class="time">{{scheduleListData.time}}</span>
+        <span class="message">
+          <span>{{scheduleListData.messageName}}</span>
+          <span>{{scheduleListData.messageNum}}</span>
+        </span>
+        <a href="javascript:;" class="button">查看进度</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:["scheduleListDatas"],
+    data () {
+      return {
+
+      }
+    },
+
+    components: {},
+
+    computed: {},
+
+    mounted(){},
+
+    methods: {}
+  }
+
+</script>
+<style lang='stylus' rel="stylesheet/stylus">
+  .scheduleList
+    li
+      box-sizing border-box
+      position relative
+      height (245/$rem)
+      padding (60/$rem) (48/$rem)
+      border-bottom 1px solid #dddddd
+      &>span
+        font-size (40/$rem)
+        color #515151
+        position absolute
+        &.bankCard
+          top (60/$rem)
+          left (48/$rem)
+        &.time
+          font-size (36/$rem)
+          top (60/$rem)
+          right (60/$rem)
+        &.message
+          font-size (36/$rem)
+          color #8a8a8a
+          bottom (60/$rem)
+          left (48/$rem)
+          span:last-child
+            margin-left (40/$rem)
+          span
+            float left
+      .button
+        position absolute
+        height (69/$rem)
+        width (300/$rem)
+        font-size (40/$rem)
+        text-align center
+        line-height (69/$rem)
+        background-color #c2181f
+        border-radius (15/$rem)
+        color #ffffff
+        bottom (46/$rem)
+        right (48/$rem)
+
+</style>
--- src/pages/schedulePage/schedulePage.vue
+++ src/pages/schedulePage/schedulePage.vue
@@ -1,20 +1,106 @@
 <template>
-  <div>schedulePage</div>
+  <div ref="scheduleWrap">
+    <div>
+      <scheduleList :scheduleListDatas="scheduleListDatas"/>
+    </div>
+  </div>
 </template>
 
 <script>
+  import scheduleList from "../../components/scheduleList/scheduleList.vue"
+  import BScroll from "better-scroll"
   export default {
     data () {
       return {
-
+        scheduleListDatas:[
+          {
+            bankCard:"交通银行卡标准信用卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+          {
+            bankCard:"交通银行白金IC航旅卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+          {
+            bankCard:"交通银行卡标准信用卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+          {
+            bankCard:"交通银行卡标准信用卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+          {
+            bankCard:"交通银行卡标准信用卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+          {
+            bankCard:"交通银行卡标准信用卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+          {
+            bankCard:"交通银行卡标准信用卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+          {
+            bankCard:"交通银行卡标准信用卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+          {
+            bankCard:"交通银行卡标准信用卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+          {
+            bankCard:"交通银行卡标准信用卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+          {
+            bankCard:"交通银行卡标准信用卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+          {
+            bankCard:"交通银行卡标准信用卡",
+            time:"2013年3月13日",
+            messageName:"程建",
+            messageNum:"22028198208260001",
+          },
+        ]
       }
     },
 
-    components: {},
+    components: {
+      scheduleList
+    },
 
     computed: {},
 
-    mounted(){},
+    mounted(){
+      this.__boxheight(this.$refs.scheduleWrap); //执行函数
+      window.onresize = this.__boxheight(this.$refs.scheduleWrap); //窗口或框架被调整大小时执行
+      this.scheduleWrap = new BScroll(this.$refs.scheduleWrap, {click: true,})
+      this.scheduleWrap.refresh()
+    },
 
     methods: {}
   }
